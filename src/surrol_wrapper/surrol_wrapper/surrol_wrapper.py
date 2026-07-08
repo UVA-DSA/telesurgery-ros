05799042d70d82fa868a8e2b442094706a22e346
Aidan Liu <aidan2023liu@gmail.com>
2026-07-08 13:22:10 -0400

    Simplify running the script

--- src/surrol_wrapper/surrol_wrapper/surrol_wrapper.py
+++ src/surrol_wrapper/surrol_wrapper/surrol_wrapper.py
@@ -1,25 +1,15 @@
-import os
-import importlib.util
 import rclpy
 from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 
-########################
-# Unused at the moment #
-########################
+from . import multiple_scenes_console_replay
+
 
 class Surrol(Node):
     def __init__(self):
         super().__init__('surrol_node')
 
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        script_path = os.path.join(current_dir, 'telesurgery-qos-analysis', 'SurRol_dVTrainer', 'tests', 'multiple_scenes_console_replay.py')
-
-        # open this file and run it here
-        # todo does not work
-        spec = importlib.util.spec_from_file_location("multiple_scenes_console_replay", script_path)
-        qos_analysis_mod = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(qos_analysis_mod)
+        multiple_scenes_console_replay.main()
 
 def main(args=None) -> None:
     try:
