0a9f7aab3fe6279add3897192c48e30d9a6c89cc
Aidan Liu <aidan2023liu@gmail.com>
2026-07-08 13:21:57 -0400

    Remove line adding package to share folder

--- src/surrol_wrapper/setup.py
+++ src/surrol_wrapper/setup.py
@@ -13,7 +13,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'telesurgery-qos-analysis'), glob('telesurgery-qos-analysis/*'))
     ],
     package_data={'': ['py.typed']},
     install_requires=['setuptools'],
