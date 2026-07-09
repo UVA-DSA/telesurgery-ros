13b86302790f99a2f51a5c4244c7e79c2a3a1a42
Aidan Liu <aidan2023liu@gmail.com>
2026-07-09 12:33:50 -0400

    Spin async and run sim on main thread

--- src/surrol_wrapper/surrol_wrapper/surrol_wrapper.py
+++ src/surrol_wrapper/surrol_wrapper/surrol_wrapper.py
@@ -1,5 +1,7 @@
+import threading
+
 import rclpy
-from rclpy.executors import ExternalShutdownException
+from rclpy.executors import ExternalShutdownException, MultiThreadedExecutor
 from rclpy.node import Node
 
 from . import multiple_scenes_console_replay
@@ -9,6 +11,7 @@ class Surrol(Node):
     def __init__(self):
         super().__init__('surrol_node')
 
+    def run_sim(self):
         multiple_scenes_console_replay.main(node=self)
 
 def main(args=None) -> None:
@@ -16,7 +19,10 @@ def main(args=None) -> None:
         with rclpy.init(args=args):
             node = Surrol()
 
-            rclpy.spin(node)
+            ros_thread = threading.Thread(target=rclpy.spin, args=(node,), daemon=True)
+            ros_thread.start()
+
+            multiple_scenes_console_replay.main(node=node)
     except (KeyboardInterrupt, ExternalShutdownException):
         pass
 
