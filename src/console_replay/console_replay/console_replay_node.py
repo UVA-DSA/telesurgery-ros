a8ff74e6cfb85cd5bfec13b90327b9a5fe6fde0c
Aidan Liu <aidan2023liu@gmail.com>
2026-07-15 12:08:12 -0400

    Declare parameters

--- src/console_replay/console_replay/console_replay_node.py
+++ src/console_replay/console_replay/console_replay_node.py
@@ -5,6 +5,7 @@ from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 from ament_index_python.packages import get_package_share_directory
 from std_msgs.msg import Bool
+from rcl_interfaces.msg import ParameterDescriptor
 
 from .replay import replayoverport
 
@@ -16,6 +17,8 @@ class ConsoleReplay(Node):
     def __init__(self):
         super().__init__('console_replay')
         package_share_dir = get_package_share_directory('console_replay')
+
+        self.init_parameters()
 
         # todo hardcoded, need to add ros2 configuration files
         data_file_path = os.path.join(
@@ -32,6 +35,39 @@ class ConsoleReplay(Node):
             self.start,
             10
         )
+
+    def init_parameters(self):
+        data_file_path_descriptor = ParameterDescriptor(
+            type=rclpy.Parameter.Type.STRING,
+            read_only=True,
+            description='File name of replay data - must be placed in replay_files directory'
+        )
+        self.declare_parameter('data_file_name', descriptor=data_file_path_descriptor)
+
+        output_mode_descriptor = ParameterDescriptor(
+            type=rclpy.Parameter.Type.STRING,
+            description='Whether to output UDP packets (UDP) or ROS2 messages (ROS)'
+        )
+        self.declare_parameter('output_mode', 'ROS', descriptor=output_mode_descriptor)
+
+        ros_topic_descriptor = ParameterDescriptor(
+            type=rclpy.Parameter.Type.STRING,
+            description='ROS topic name'
+        )
+        self.declare_parameter('ros_topic_name', '/itp_commands', descriptor=ros_topic_descriptor)
+
+        udp_ip_descriptor = ParameterDescriptor(
+            type=rclpy.Parameter.Type.STRING,
+            description='UDP IP address'
+        )
+        self.declare_parameter('udp_ip', '127.0.0.1', descriptor=udp_ip_descriptor)
+
+        udp_port_descriptor = ParameterDescriptor(
+            type=rclpy.Parameter.Type.INTEGER,
+            description='UDP port number'
+        )
+        self.declare_parameter('udp_port', 5001, descriptor=udp_port_descriptor)
+
 
     def start(self, msg: Bool):
         if not msg.data: return
