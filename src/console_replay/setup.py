5449288e430ddb2f058525d82050dcc2125d93ac
Aidan Liu <aidan2023liu@gmail.com>
2026-07-15 13:44:30 -0400

    Have config work when launching full-stack

--- src/console_replay/setup.py
+++ src/console_replay/setup.py
@@ -13,7 +13,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'replay_files'), glob('replay_files/*'))
+        (os.path.join('share', package_name, 'replay_files'), glob('replay_files/*')),
+        (os.path.join('share', package_name, 'param'), glob('param/*')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
     ],
     package_data={'': ['py.typed']},
     install_requires=['setuptools'],
