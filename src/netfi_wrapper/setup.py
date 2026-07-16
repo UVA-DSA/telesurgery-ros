6db077344219609b832db810c2ab55bfbfdcfbd0
Aidan Liu <aidan2023liu@gmail.com>
2026-07-16 18:57:15 -0400

    Attempt to use delay emulator

--- src/netfi_wrapper/setup.py
+++ src/netfi_wrapper/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = 'netfi_wrapper'
@@ -10,6 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'param'), glob('param/*')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
     ],
     package_data={'': ['py.typed']},
     install_requires=['setuptools'],
