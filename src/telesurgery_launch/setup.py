e4305d8e737a982b21a31965bd4abdeec43f8762
Aidan Liu <aidan2023liu@gmail.com>
2026-07-16 17:34:10 -0400

    Add market file so colcon does not complain

--- src/telesurgery_launch/setup.py
+++ src/telesurgery_launch/setup.py
@@ -9,6 +9,8 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         # launch files
         (os.path.join('share', package_name, 'launch'), glob('launch/*')),
