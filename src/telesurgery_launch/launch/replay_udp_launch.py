5e9382c35f6bba38be7d93e3ed8e4dff618bf047
Aidan Liu <aidan2023liu@gmail.com>
2026-07-20 12:00:58 -0400

    Remove extra launch files on network node

--- src/telesurgery_launch/launch/replay_udp_launch.py
+++ src/telesurgery_launch/launch/replay_udp_launch.py
@@ -27,9 +27,12 @@ def generate_launch_description():
             PythonLaunchDescriptionSource(
                 get_share_file(
                     package_name="network_interface",
-                    file_path="launch/network_interface_launch_udp.py"
+                    file_path="launch/network_interface_launch.py"
                 )
-            )
+            ),
+            launch_arguments={
+                'config': 'udp'
+            }.items(),
         ),
 
         Node(
