978d868e5bd3d34d0b04d56b64803a019243212a
Aidan Liu <aidan2023liu@gmail.com>
2026-07-28 15:58:57 -0400

    Send dummy packets to /agent/itp_commands

--- src/recovery/recovery/autonomy_engine.py
+++ src/recovery/recovery/autonomy_engine.py
@@ -2,15 +2,24 @@ import rclpy
 from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 
+from teleop_msgs.msg import ITP
+
+
 class AutonomyEngine(Node):
     def __init__(self):
         super().__init__('autonomy_engine')
+        self.publisher = self.create_publisher(ITP, '/agent/itp_commands', 100)
+        self.timer = self.create_timer(0.1, self.publish_dummy_itp)
 
+    def publish_dummy_itp(self):
+        msg = ITP()
+        self.publisher.publish(msg)
 
 def main(args=None):
     try:
         with rclpy.init(args=args):
             node = AutonomyEngine()
+            node.get_logger().info("Starting dummy autonomy engine. This sends agent ITP packets to /agent/itp_commands")
 
             rclpy.spin(node)
     except (KeyboardInterrupt, ExternalShutdownException):
