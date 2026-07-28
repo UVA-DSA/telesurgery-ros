d61e943af2d5c6a08591cd4162866002363e144f
Aidan Liu <aidan2023liu@gmail.com>
2026-07-28 16:40:10 -0400

    Do not send agent ITP commands if state is normal operation

--- src/recovery/recovery/autonomy_engine.py
+++ src/recovery/recovery/autonomy_engine.py
@@ -2,22 +2,28 @@ import rclpy
 from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 
-from teleop_msgs.msg import ITP
+from teleop_msgs.msg import ITP, RecoveryStatus
 
 
 class AutonomyEngine(Node):
     def __init__(self):
         super().__init__('autonomy_engine')
         self.publisher = self.create_publisher(ITP, '/agent/itp_commands', 100)
+        self.state_listener = self.create_subscription(RecoveryStatus, '/recovery_type', self.recovery_callback, 10)
         self.timer = self.create_timer(0.5, self.publish_dummy_itp)
         self.grasper = True
+        self.state = RecoveryStatus.NORMAL_OPERATION
 
     # Open and close grasper0 every half second
     def publish_dummy_itp(self):
+        if self.state == RecoveryStatus.NORMAL_OPERATION: return
         msg = ITP()
         msg.grasp0 = self.grasper
         self.publisher.publish(msg)
         self.grasper = not self.grasper
+
+    def recovery_callback(self, msg: RecoveryStatus):
+        self.state = msg.state
 
 def main(args=None):
     try:
