41180ab8be79fe566264878cdca36093791193db
Aidan Liu <aidan2023liu@gmail.com>
2026-08-02 16:00:03 -0400

    Create profiler

--- src/network_monitor/network_monitor/network_monitor.py
+++ src/network_monitor/network_monitor/network_monitor.py
@@ -1,9 +1,11 @@
 import random
 
 import rclpy
+from rcl_interfaces.srv import GetParameters
 from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 
+from network_monitor.profiler import Profiler
 from teleop_msgs.srv import NetworkStatistics
 
 
@@ -15,6 +17,8 @@ class NetworkMonitor(Node):
         self.create_service(NetworkStatistics, "/network_statistics", self.statistics_callback)
 
         self.profiler_enabled = self.get_parameter('profiler').get_parameter_value().bool_value
+
+        self.profiler = None
 
 
     def statistics_callback(self, request, response: NetworkStatistics):
@@ -29,6 +33,10 @@ def main(args=None):
             node = NetworkMonitor()
 
             node.get_logger().info("Starting dummy network monitor. This hosts a service that returns network statistics on request.")
+
+            if node.profiler_enabled:
+                node.get_logger().info("Starting profiler.")
+                node.profiler = Profiler(node)
 
             rclpy.spin(node)
     except (KeyboardInterrupt, ExternalShutdownException):
